8c2daee19e88f161c359cf6425ed346983ef33f1
Brian Riddle <brianjriddle@gmail.com>
2024-02-28 20:07:59 +0100

    telekasten -> orgmode(again)

--- lua/custom/plugins/luasnip.lua
+++ lua/custom/plugins/luasnip.lua
@@ -3,6 +3,6 @@ return {
   dependencies = { 'rafamadriz/friendly-snippets' },
   config = function()
     require('luasnip.loaders.from_vscode').lazy_load()
-    require('luasnip.loaders.from_vscode').lazy_load { paths = { '~/.code-snippets.vscode.json' } }
+    require('luasnip.loaders.from_vscode').lazy_load { paths = { '~/org/snippets/' } }
   end,
 }
