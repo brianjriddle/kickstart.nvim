4092890c2d734ed8a3cbc5338ea015a3b6b0ea77
Brian Riddle <brianjriddle@gmail.com>
2024-03-02 10:59:03 +0100

    Add css, gopls, html, jqls, marksman

--- init.lua
+++ init.lua
@@ -535,7 +535,11 @@ require('lazy').setup {
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
+        cssls = {},
         gopls = {},
+        html = {},
+        jqls = {},
+        marksman = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
