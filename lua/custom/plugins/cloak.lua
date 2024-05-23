40c46f0f6b292ad0efe658bc48a785d861bff1a6
Brian Riddle <brian.riddle@avanza.se>
2024-05-23 12:33:19 +0200

    Add cloak

---
+++ lua/custom/plugins/cloak.lua
@@ -0,0 +1,17 @@
+return {
+  'laytan/cloak.nvim',
+  config = function()
+    require('cloak').setup {
+      enable = true,
+      cloak_character = '*',
+      hightlight_group = 'Comment',
+      cloak_length = nil,
+      patterns = {
+        {
+          file_pattern = '*.env*',
+          cloak_pattern = '=.+',
+        },
+      },
+    }
+  end,
+}
